004895cd6428d1edf67ede79071b596067ca8ad0
willmaier <maier.w@northeastern.edu>
2023-03-30 20:39:47 -0400

    Basic movement. Press 3 & 7 on numpad to jump

---
+++ Button Game/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    private BoxCollider2D coll;
+    private SpriteRenderer sprite;
+    private Animator anim;
+
+    [SerializeField] private LayerMask jumpableGround;
+
+    private float dirX = 0f;
+    [SerializeField] private float moveSpeed = 7.5f;
+    [SerializeField] private float jumpForce = 10f;
+
+    private enum MovementState { idle, jumping, falling }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        coll = GetComponent<BoxCollider2D>();
+        sprite = GetComponent<SpriteRenderer>();
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        dirX = Input.GetAxisRaw("Horizontal");
+
+        rb.velocity = new Vector2(dirX * moveSpeed, rb.velocity.y);
+
+        // traditional 'spacebar' input is commented out
+        // if (Input.GetButtonDown("Jump") && IsGrounded())
+            if (Input.GetKey(KeyCode.Keypad3) && Input.GetKey(KeyCode.Keypad7) && IsGrounded())
+        {
+            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+        }
+    }
+
+    private bool IsGrounded()
+    {
+        return Physics2D.BoxCast(coll.bounds.center, coll.bounds.size, 0f, Vector2.down, 0.1f, jumpableGround);
+    }
+
+    // better way to check for button presses?
+    /* private bool isJumping()
+    {
+        if (Input.GetKey(KeyCode.Keypad3)
+        {
+            return true;
+        }
+    } */
+}
