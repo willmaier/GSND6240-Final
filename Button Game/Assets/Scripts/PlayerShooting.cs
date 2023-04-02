20b55eb3c0cc59c2229793479f70e55d3d2921bc
willmaier <maier.w@northeastern.edu>
2023-04-02 14:36:59 -0400

    Player prefab, added shooting script

---
+++ Button Game/Assets/Scripts/PlayerShooting.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerShooting : MonoBehaviour
+{
+    public GameObject shot, chargedShot, specialShot;
+
+    public float shotSpeed = 5.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
