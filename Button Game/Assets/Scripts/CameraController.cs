8c875bbaf6e3f396e3b6584de27928e454c5520c
Wuzh-9 <129639585+Wuzh-9@users.noreply.github.com>
2023-04-03 01:41:14 -0400

    Using Vector3.SmoothDamp to smoothly move the camera.
    Still not moving in the ideal Cave Story fashion though. Further edits are needed.

--- Button Game/Assets/Scripts/CameraController.cs
+++ Button Game/Assets/Scripts/CameraController.cs
@@ -5,26 +5,28 @@ using UnityEngine;
 public class CameraController : MonoBehaviour
 {
     public GameObject player;
-    private PlayerMovement playerMovementScript;
     private Transform playerTransform;
+
+    public float smoothTime = 0.3f;
+    public float cameraDistance = 10.0f;
+
+    private Vector3 velocity = Vector3.zero;
 
     // Start is called before the first frame update
     void Start()
     {
-        playerMovementScript = player.GetComponent<PlayerMovement>();
         playerTransform = player.GetComponent<Transform>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (playerMovementScript.facingRight)
-        {
-            transform.position = new Vector3(playerTransform.position.x + 4, playerTransform.position.y + 1, -10);
-        }
-        else
-        {
-            transform.position = new Vector3(playerTransform.position.x - 4, playerTransform.position.y + 1, -10);
-        }
+        // Set the camera's target position in front of the player
+        Vector3 targetPosition = playerTransform.position + playerTransform.forward * cameraDistance;
+        targetPosition.y = playerTransform.position.y + 1; // Lock the camera's height at player's y vector + 1
+        targetPosition.z = -10;
+
+        // Smoothly move the camera to the target position
+        transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref velocity, smoothTime);
     }
 }
