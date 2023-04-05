ba65608d5befede795f384f215a6f520d2e21ff1
Wuzh-9 <129639585+Wuzh-9@users.noreply.github.com>
2023-04-04 20:51:53 -0400

    Made the camera a child of the player character

--- Button Game/Assets/Scripts/CameraController.cs
+++ Button Game/Assets/Scripts/CameraController.cs
@@ -4,9 +4,6 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
-    public GameObject player;
-    private Transform playerTransform;
-
     public float smoothTime = 0.5f;
     public float cameraDistance = 5.0f;
 
@@ -17,18 +14,17 @@ public class CameraController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        playerTransform = player.GetComponent<Transform>();
     }
 
     // Update is called once per frame
     void Update()
     {
         // Set the camera's target horizontal position in front of the player, direction based on the player's inputs
-        targetPosition.x = playerTransform.position.x + (cameraDistance * Input.GetAxisRaw("Horizontal"));
+        targetPosition.x = cameraDistance * Input.GetAxisRaw("Horizontal");
 
         // Create a dampened vector; we'll only be using the x value of the dampened vector
-        Vector3 smoothedPosition = Vector3.SmoothDamp(transform.position, targetPosition, ref velocity, smoothTime);
+        Vector3 smoothedPosition = Vector3.SmoothDamp(transform.localPosition, targetPosition, ref velocity, smoothTime);
 
-        transform.position = new Vector3(smoothedPosition.x, playerTransform.position.y + 1, -10);
+        transform.localPosition = new Vector3(smoothedPosition.x, 1, -10);
     }
 }
