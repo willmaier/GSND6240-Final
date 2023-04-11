5f7a4d6e4fed598b43c90a096797f1a50a85a735
willmaier <maier.w@northeastern.edu>
2023-04-11 17:30:05 -0400

    Added respawning after falling into hole

---
+++ Button Game/Assets/Scripts/Enemy Scripts/DeadzoneRespawn.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeadzoneRespawn : MonoBehaviour
+{
+    public Transform respawnPoint;
+    GameObject player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            player.transform.position = respawnPoint.position;
+        }
+    }
+}
