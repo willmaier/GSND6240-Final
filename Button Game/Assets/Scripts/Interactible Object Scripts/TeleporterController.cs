b57de2b6fcd20a3067d06d73ed33b50a2b4dfe2b
willmaier <maier.w@northeastern.edu>
2023-04-25 17:53:06 -0400

    Changed teleporters to automatically activate

--- Button Game/Assets/Scripts/Interactible Object Scripts/TeleporterController.cs
+++ Button Game/Assets/Scripts/Interactible Object Scripts/TeleporterController.cs
@@ -2,12 +2,20 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class TeleporterController : MonoBehaviour, IInteractible
+public class TeleporterController : MonoBehaviour
 {
     public GameObject player;
     public Transform teleportTarget;
 
-    public void OnInteract()
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            Invoke("Teleport", 3);
+        }
+    }
+
+    public void Teleport()
     {
         Debug.Log("Teleporting!");
         player.transform.position = teleportTarget.transform.position;
